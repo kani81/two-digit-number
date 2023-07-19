2f0065921d1587474cc2b1ff81d4936a44617361
kani81 <kaniarasiduraisamy.com>
2023-07-19 15:33:15 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,7 @@
+var arr=[11,10,70,75,59,2,4];
+document.write("arr=[11,10,70,75,59,2,4]"+"<br>"+"<br>");
+for(let i=0;i<arr.length;i++){
+if((arr[i]>9)&&(arr[i]<99)){
+	document.write(arr[i]);
+}
+}
